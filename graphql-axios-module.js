36617eecaaa12f13ef97697ac3ddee5b2988643b
Rulo Kobashikawa <akobashikawa@gmail.com>
2018-02-19 11:53:29 -0500

    Add graphql axios module case

---
+++ graphql-axios-module.js
@@ -0,0 +1,25 @@
+const axios = require("axios");
+
+const url = "http://localhost:4200/graphql";
+// const data = JSON.stringify({ query: `query { beta }` });
+const headers = {
+    Accept: "application/json, text/plain, */*",
+    "Content-Type": "application/json"
+};
+
+const getResult = async (url) => {
+    try {
+        const response = await axios({
+            url,
+            headers,
+            method: "POST",
+            data: JSON.stringify({ query: `query { beta }` }) // not work as const
+        });
+        const data = response.data;
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+getResult(url);
