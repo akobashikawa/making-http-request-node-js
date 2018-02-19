93667160d3cda741737b69e588b92099f47ac08e
Rulo Kobashikawa <akobashikawa@gmail.com>
2018-02-19 11:53:15 -0500

    Add graphql-node-fetch case

---
+++ graphql-node-fetch.js
@@ -0,0 +1,24 @@
+const fetch = require("node-fetch");
+
+const url = "http://localhost:4200/graphql";
+const data = JSON.stringify({ query: `query { beta }` });
+const headers = {
+    Accept: "application/json, text/plain, */*",
+    "Content-Type": "application/json"
+};
+
+const getResult = async (url) => {
+    try {
+        const response = await fetch(url, {
+            headers,
+            method: "POST",
+            body: data
+        });
+        const json = await response.json();
+        console.log(json);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+getResult(url);
